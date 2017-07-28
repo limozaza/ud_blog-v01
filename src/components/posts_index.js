31d263e753a01fb8869bdccb7ab48588f4ffab70
limozaza <zakboite@gmail.com>
2017-07-28 16:20:30 +0200

    Clean project and add first route

---
+++ src/components/posts_index.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class PostsIndex extends Component{
+    render(){
+        return (
+            <div>
+                PostsIndex
+            </div>
+        );
+    }
+}
+export default PostsIndex;
